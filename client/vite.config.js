f97f5b6f94f8e32d7bd750b97af34fd85c347296
Cyp-cyp-22 <2628076161@qq.com>
2026-04-23 15:10:30 +0800

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ import { fileURLToPath, URL } from 'node:url'
 export default defineConfig({
   plugins: [vue()],
   // 核心：相对路径base，本地双击index.html必须配置
-  base: '/vue-bridge/',
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
